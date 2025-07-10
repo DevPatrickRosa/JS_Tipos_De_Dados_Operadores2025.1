f06a53e5e0c31f4470ab1f15c2610c54752e7bd2
DevPatrickRosa <patrick.dejesus.pj@gmail.com>
2025-07-09 22:29:40 -0300

    Console-para-JS
    Conhecendo as funções em JS.

---
+++ JS/script.js
@@ -0,0 +1,85 @@
+//Formas de comentar em JS
+//Usamos o "//"para comentar em uma linha
+//Usamos "/**/" paracomentar mais de uma linha.
+
+/* Exemplo de comentarios:
+ O pé do Padre Pedro é Preto.*/
+
+ // Testando o JS
+ //console.log('Fala galera Bora lançar um Hello Word?');
+
+ //Tipos de Dados:
+
+ // 1- Number
+ console.log(typeof 2);
+ console.log(typeof 2.2);
+ console.log(typeof -2);
+ 
+ // 2 - Operações Atiriméticas
+
+ console.log(2 +4); //6
+ console.log(10 -5); //5
+ console.log(5 * 4); //20
+ console.log(10 / 2); //5
+ console.log(5* (5+3)); //40
+
+
+ // 3 - Special Numbers
+
+ console.log(typeof Infinity); //nuber
+
+ console.log(typeof -Infinity); //number
+
+ console.log(12*"asd"); //NaN
+
+ console.log(typeof NaN);
+ // 4 - Strings
+
+ console.log(typeof "Um texto qualquer.");
+
+ console.log(typeof 'Mais umtexto.');
+
+ console.log(typeof `Agora mais um texto diferente.`);
+
+
+ // 5 - Símbolos Especiais em String
+ console.log('Testando\n quebra de linha');
+
+ console.log('Espaçamento \tde tab');
+ 
+ // 6- Concatenação
+ 
+console.log("Oi" + ' tudo' + ' bem?');
+
+console.log('Testando' + ' Concatenação.');
+
+// Exemplos de Interpolação
+
+const nome = "Marcão";
+const idade = 40;
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos.`);//interpolação.
+console.log("Meu nome é " + nome + " e eu tenho " + idade + " anos.");
+
+// 7 - Interpolação
+
+console.log(`A soma de 2 + 2 é: ${2+2}`);
+console.log(`A multiplicação de 3 * 3 é: ${3*3}`);
+console.log("Vamos testar a concatenação com a " + `interpolação para ver se fica ${5+5}`);//mix de concatenação e interpolação
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
